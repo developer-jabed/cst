5d9336e564924a4d32ad44a805616fc59f852ee6
developer-jabed <jabed1780@gmail.com>
2026-05-15 07:52:02 +0600

    added dynamic to root

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,9 +1,10 @@
-import HomePage from '@/components/shared/HomePage'
+import HomePage from "@/components/shared/HomePage";
+export const dynamic = "force-dynamic";
 
 export default function home() {
   return (
     <div>
       <HomePage />
     </div>
-  )
+  );
 }
